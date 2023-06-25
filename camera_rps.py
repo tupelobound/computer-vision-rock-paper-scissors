e52bce6c4438fa6b2bff74a473c1f38ddb1a26be
tupelobound <29886944+tupelobound@users.noreply.github.com>
2023-06-25 18:39:25 +0100

    Creates RPS class and adds get_prediction function

--- camera_rps.py
+++ camera_rps.py
@@ -1,61 +1,121 @@
 import random # import random module to use choice() method
+from keras.models import load_model  # TensorFlow is required for Keras to work
+import cv2  # Requires opencv-python
+import numpy as np
 
-def get_computer_choice():
-    '''Returns a random choice - either 'rock', 'paper', or 'scissors'.'''
-    # use random.choice() to randomly select and return one of the strings
-    return random.choice(['rock', 'paper', 'scissors'])
+# Disable scientific notation for clarity
+np.set_printoptions(suppress=True)
 
-def get_user_choice():
+class RPS:
     '''
-    Returns user choice of either 'rock', 'paper', or 'scissors'.
-
-    Takes user input and checks that it is valid. Will repeatedly ask for input if user does not input 'rock', 'paper' or 'scissors'.
-    Converts user input to lowercase, so capitalization is fine.
+    A class that represents an instance of the computer vision rock-paper-scissors game
     '''
-    # ask user to input their choice and convert to lowercase
-    user_choice = input("Please enter your choice of rock, paper or scissors: ").lower()
-    # start loop
-    while True:
-        # check if user input is valid
-        if user_choice in ['rock', 'paper', 'scissors']:
-            # if so, end loop and return user_choice
-            return user_choice
-        else:
-            # otherwise, ask for input again and convert to lowercase
-            user_choice = input("Sorry, that isn't a valid input. Please enter either rock, paper or scissors: ").lower()
+    def __init__(self):
+        pass
 
-def get_winner(computer_choice, user_choice):
-    '''
-    Takes two strings, compares them and evaluates who the winner is based on the rules of rock-paper-scissors.
 
-    Parameters
-    ----------
-    computer_choice : str
-        A string generated from running the function get_computer_choice
-    user_choice : str
-        A string generated from running the function get_user_choice
+    def get_computer_choice(self):
+        '''Returns a random choice - either 'rock', 'paper', or 'scissors'.'''
+        # use random.choice() to randomly select and return one of the strings
+        return random.choice(['rock', 'paper', 'scissors'])
+
+
+    def get_user_choice(self):
+        '''
+        Returns user choice of either 'rock', 'paper', or 'scissors'.
+
+        Takes user input and checks that it is valid. Will repeatedly ask for input if user does not input 'rock', 'paper' or 'scissors'.
+        Converts user input to lowercase, so capitalization is fine.
+        '''
+        # ask user to input their choice and convert to lowercase
+        user_choice = input("Please enter your choice of rock, paper or scissors: ").lower()
+        # start loop
+        while True:
+            # check if user input is valid
+            if user_choice in ['rock', 'paper', 'scissors']:
+                # if so, end loop and return user_choice
+                return user_choice
+            else:
+                # otherwise, ask for input again and convert to lowercase
+                user_choice = input("Sorry, that isn't a valid input. Please enter either rock, paper or scissors: ").lower()
+
+
+    def get_winner(self, computer_choice, user_choice):
+        '''
+        Takes two strings, compares them and evaluates who the winner is based on the rules of rock-paper-scissors.
+
+        Parameters
+        ----------
+        computer_choice : str
+            A string generated from running the function get_computer_choice
+        user_choice : str
+            A string generated from running the function get_user_choice
         
-    Returns
-    -------
-    None
-    '''
-    # check all three conditions whereby computer can win
-    if (computer_choice == 'rock' and user_choice == 'scissors') or \
-       (computer_choice == 'paper' and user_choice == 'rock') or \
-       (computer_choice == 'scissors' and user_choice == 'paper'):
-        # if so, print losing message to console
-        print("You lost")
-    # check if user_choice and computer_choice are identical
-    elif computer_choice == user_choice:
-        # if so, print tie message to console
-        print("It is a tie!")
-    else:
-        # otherwise, print winning message to console
-        print("You won!")
+        Returns
+        -------
+        None
+        '''
+        # check all three conditions whereby computer can win
+        if (computer_choice == 'rock' and user_choice == 'scissors') or \
+           (computer_choice == 'paper' and user_choice == 'rock') or \
+           (computer_choice == 'scissors' and user_choice == 'paper'):
+            # if so, print losing message to console
+            print("You lost")
+        # check if user_choice and computer_choice are identical
+        elif computer_choice == user_choice:
+            # if so, print tie message to console
+            print("It is a tie!")
+        else:
+            # otherwise, print winning message to console
+            print("You won!")
+    
 
-def play():
-    '''Runs one round of the game rock-paper-scissors.'''
-    get_winner(get_computer_choice(), get_user_choice())
+    def get_prediction(self):
+        # Load the model
+        model = load_model("keras_Model.h5", compile=False)
 
-# run play() function to test code
-play()
+        # Load the labels
+        class_names = open("labels.txt", "r").readlines()
+
+        # CAMERA can be 0 or 1 based on default camera of your computer
+        camera = cv2.VideoCapture(0)
+
+        while True:
+            # Grab the webcamera's image.
+            ret, image = camera.read()
+
+            # Resize the raw image into (224-height,224-width) pixels
+            image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+            # Show the image in a window
+            cv2.imshow("Webcam Image", image)
+
+            # Make the image a numpy array and reshape it to the models input shape.
+            image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+            # Normalize the image array
+            image = (image / 127.5) - 1
+
+            # Predicts the model
+            prediction = model.predict(image)
+            index = np.argmax(prediction)
+            class_name = class_names[index]
+            confidence_score = prediction[0][index]
+
+            # Print prediction and confidence score
+            print("Class:", class_name[2:], end="")
+            print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+            # Listen to the keyboard for presses.
+            keyboard_input = cv2.waitKey(1)
+
+            # 27 is the ASCII for the esc key on your keyboard.
+            if keyboard_input == 27:
+                break
+
+        camera.release()
+        cv2.destroyAllWindows()
+        return prediction
+    
+game = RPS()
+print(game.get_prediction())
